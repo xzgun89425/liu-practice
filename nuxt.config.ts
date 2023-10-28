f6137d06a7736910c9b72ef1f9a52d0117f59220
xzgun89425 <68765411+xzgun89425@users.noreply.github.com>
2023-10-28 23:32:23 +0800

    anita

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  "app": {
+    "head": {
+      "viewport": "width=device-width, initial-scale=1",
+      "title": "Anita",
+      "meta": [
+          { "name": "description", "content": "這是 Anita" },
+          { "property": "og:title", "content": "這是 Anita" },
+          { "property": "og:description", "content": "這是 Anita" },
+      ]
+    }
+  },
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
